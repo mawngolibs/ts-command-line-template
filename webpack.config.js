d42ad9d3005d67148eb858cdb5aeb190b382fbdc
Lana <sitdownrightnow2552@gmail.com>
2022-01-17 00:19:09 +0700

    chore: update webpack config and build command

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
-const meta = require('./package.json');
+const path = require("path");
+const meta = require("./package.json");
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'production',
-    target: 'node',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-                include: path.resolve(__dirname, 'src'),
-            },
-        ],
-    },
-    resolve: {
-        mainFields: ['main', 'module'],
-        extensions: ['.tsx', '.ts', '.js', '.json'],
-    },
-    output: {
-        filename: `${meta.name}.bundle.js`,
-    },
+  entry: "./src/index.ts",
+  mode: "production",
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, "src")
+      }
+    ]
+  },
+  resolve: {
+    mainFields: ["main", "module"],
+    extensions: [".tsx", ".ts", ".js", ".json"]
+  },
+  output: {
+    filename: `${meta.name}.bundle.js`
+  }
 };
