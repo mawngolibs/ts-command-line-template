3398ca5b20feb88dc85d95b42bcadf3b12040a3d
Lana <sitdownrightnow2552@gmail.com>
2022-01-17 00:10:08 +0700

    style: add eslint and editor config (webstorm ver)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import hello from './commands/hello';
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import hello from "./commands/hello";
 
 yargs(hideBin(process.argv))
-    .command(hello)
-    .argv;
+  .command(hello)
+  .argv;
