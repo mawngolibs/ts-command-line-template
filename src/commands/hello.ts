bb01179387b09941ddb14d64ecb9025b0af993cf
Lana <sitdownrightnow2552@gmail.com>
2022-04-29 19:14:34 +0700

    fix: better example command typing

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,14 +1,15 @@
 import { CommandModule } from "yargs";
 import chalk from "chalk";
 
-const command: CommandModule = {
+const command: CommandModule<unknown, { hi: boolean }> = {
   command: ["index", "$0"],
   describe: "Sample command",
   handler: (args) => console.log(chalk.green(`${args.hi ? "Hi" : "Hello"} World!`)),
   builder: (yargs) => yargs
     .option("hi", {
       describe: "Say 'Hi' instead",
-      boolean: true
+      boolean: true,
+      default: false
     })
 };
 
